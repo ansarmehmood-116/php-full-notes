13111e5bee80aea99da2067162b79440a74f5318
Ansar Mehmood <ansarkhanmehmood@gmail.com>
2023-12-08 10:03:40 +0500

    second time committing

--- tariq-php-lect1.php
+++ tariq-php-lect1.php
@@ -2,8 +2,23 @@
     for($i=0;$i<10;$i++){
     // when we write (i) in double quots so it will give us the number exactly (same is the case for without double quotation) and if we use single quote for (i) so it will as it is print the (i)
 
-    echo $i;      //  output 0 to 9
-    echo "$i";      //  output 0 to 9
-    echo '$i';      //  output 10 times i
+    // echo $i;      //  output 0 to 9
+    // echo "$i";      //  output 0 to 9
+    // echo '$i';      //  output 10 times i
     }
+
+    // ---------------------------------------------------------------
+
+
+    $x=10;
+    $y=20;
+
+    $z=$x+$y;
+    echo "the sume is ".$z;
+    echo "<br>the subtraction is".$y-$x;
+    print("<br> multiplaction is ".$x*$y);
+
+
+    // ---------------------------------------------------------------
+
 ?>
