61ad2a3196b6fc0751bfc524694bc251889507ef
Ansar Mehmood <ansarkhanmehmood@gmail.com>
2023-12-08 09:55:05 +0500

    committing first time

---
+++ tariq-php-lect1.php
@@ -0,0 +1,9 @@
+<?php
+    for($i=0;$i<10;$i++){
+    // when we write (i) in double quots so it will give us the number exactly (same is the case for without double quotation) and if we use single quote for (i) so it will as it is print the (i)
+
+    echo $i;      //  output 0 to 9
+    echo "$i";      //  output 0 to 9
+    echo '$i';      //  output 10 times i
+    }
+?>
